034bc5c231206f7a8f6b53d98857730188abeeb9
hicham-darw <hamoudhicham234@gmail.com>
2025-10-04 23:46:05 +0100

    found jewelleries

---
+++ found_collectibles.c
@@ -0,0 +1,19 @@
+int	found_collectibles(char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'C')
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
