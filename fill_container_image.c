355f153475e3d23839f2fe14b8224bebefaf3710
hicham-darw <hamoudhicham234@gmail.com>
2025-10-07 21:26:22 +0100

    fill container pixels

--- fill_container_image.c
+++ fill_container_image.c
@@ -10,11 +10,24 @@ void	fill_container_image(void *mlx,t_data_img *s_img, t_data_img *d_img, int wi
 
    	d_img->img = mlx_new_image(mlx, window_sx / width_map, window_sy / height_map);
 	d_img->addr = mlx_get_data_addr(d_img->img, &d_img->bpp, &d_img->line_length, &d_img->endian);
+	if (!d_img->addr)
+	{
+		printf("error dst img\n");
+		return ;
+	}
+	printf("|%u|\n", *(unsigned int *)d_img->addr);
 
 	s_img->img = mlx_xpm_file_to_image(mlx, s_img->xpm_file, &s_img->xpm_width, &s_img->xpm_height);
 	s_img->addr = mlx_get_data_addr(s_img->img, &s_img->bpp, &s_img->line_length, &s_img->endian);
 
-    i = 0;
+	if (!s_img->addr)
+	{
+		printf("error dst img2\n");
+		return ;
+	}
+	printf("|%u|\n", *(unsigned int *)d_img->addr);
+
+	i = 0;
 	while (i < (window_sy / height_map))
 	{
 		j = 0;
@@ -25,7 +38,7 @@ void	fill_container_image(void *mlx,t_data_img *s_img, t_data_img *d_img, int wi
 
 			src_pixel = s_img->addr + (src_y * s_img->line_length) + (src_x * (s_img->bpp / 8));
 			dst_pixel = d_img->addr + (i * d_img->line_length) + (j * (d_img->bpp / 8));
-			
+
 			*(unsigned int *)dst_pixel = *(unsigned int *)src_pixel;
 			j++;
 		}
