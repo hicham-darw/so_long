60db97eda03a6c6faffa6f9f9a103becfa2d590f
hicham-darw <hamoudhicham234@gmail.com>
2025-10-07 21:25:08 +0100

    put_map

--- put_map_to_window.c
+++ put_map_to_window.c
@@ -3,33 +3,30 @@
 #include "libso_long.h"
 #include <fcntl.h>
 
-int		put_map_to_window(void *mlx, void *new_win, char **map, t_data_img *dst_img, int window_sx, int window_sy)
+int		put_map_to_window(t_allvars *vars)
 {
-    char    *line;
     int		i, j;
-	int	width_map, height_map;
 
-    if (!mlx || !new_win || !map || !dst_img)
-        return (0);
-	width_map = ft_strlen(map[0]);
-	height_map = ft_veclen(map);
-
+	if (!vars->map || !*vars->map)
+		return (0);
+	vars->width_map = ft_strlen(vars->map[0]);
+	vars->height_map = ft_veclen(vars->map);
 	i = 0;
-	while (map[i])
+	while (vars->map[i])
 	{
 		j = 0;
-		while (map[i][j])
+		while (vars->map[i][j])
 		{
-			if (map[i][j] == '1')
-				mlx_put_image_to_window(mlx, new_win, (dst_img + 1)->img, (window_sx / width_map) * j, (window_sy / height_map) * i);
-			else if (map[i][j] == '0')
-				mlx_put_image_to_window(mlx, new_win, dst_img[0].img, (window_sx / width_map) * j, (window_sy / height_map) * i);
-			else if (map[i][j] == 'P')
-				mlx_put_image_to_window(mlx, new_win, dst_img[4].img, (window_sx / width_map) * j, (window_sy / height_map) * i);
-			else if (map[i][j] == 'C')
-				mlx_put_image_to_window(mlx, new_win, dst_img[2].img, (window_sx / width_map) * j, (window_sy / height_map) * i);
-			else if (map[i][j] == 'E')
-				mlx_put_image_to_window(mlx, new_win, dst_img[3].img, (window_sx / width_map) * j, (window_sy / height_map) * i);
+			if (vars->map[i][j] == '1')
+				mlx_put_image_to_window(vars->mlx, vars->window, vars->data_img[1].img, (vars->window_sx / vars->width_map) * j, (vars->window_sy / vars->height_map) * i);
+			else if (vars->map[i][j] == '0')
+				mlx_put_image_to_window(vars->mlx, vars->window, vars->data_img[0].img, (vars->window_sx / vars->width_map) * j, (vars->window_sy / vars->height_map) * i);
+			else if (vars->map[i][j] == 'P')
+				mlx_put_image_to_window(vars->mlx, vars->window, vars->data_img[4].img, (vars->window_sx / vars->width_map) * j, (vars->window_sy / vars->height_map) * i);
+			else if (vars->map[i][j] == 'C')
+				mlx_put_image_to_window(vars->mlx, vars->window, vars->data_img[2].img, (vars->window_sx / vars->width_map) * j, (vars->window_sy / vars->height_map) * i);
+			else if (vars->map[i][j] == 'E')
+				mlx_put_image_to_window(vars->mlx, vars->window, vars->data_img[3].img, (vars->window_sx / vars->width_map) * j, (vars->window_sy / vars->height_map) * i);
 			else
 				printf("invalid img!");
 			j++;
