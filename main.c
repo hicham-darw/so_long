bf7333bb9d2d1e40a8e6aa16c8f406603f10da2b
hicham-darw <hamoudhicham234@gmail.com>
2025-10-09 18:35:54 +0100

    main function

--- main.c
+++ main.c
@@ -18,13 +18,6 @@ int	main(int ac, char **av)
 		printf("Usage: ./program_name file.ber\n");
 		exit(EXIT_FAILURE);
 	}
-	//invalid map function here!!!!
-	s_data_img[0].xpm_file = ft_strdup("textures/floor.xpm");
-	s_data_img[1].xpm_file = ft_strdup("textures/wall.xpm");
-	s_data_img[2].xpm_file = ft_strdup("textures/jewellery.xpm");
-	s_data_img[3].xpm_file = ft_strdup("textures/exit.xpm");
-	s_data_img[4].xpm_file = ft_strdup("textures/player_left.xpm");
-	s_data_img[5].xpm_file = ft_strdup("textures/player_right.xpm");
 
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
@@ -40,6 +33,34 @@ int	main(int ac, char **av)
 		free(line_map);
 	}
 	close(fd);
+	allvars.map = get_map(av[1], allvars.height_map);
+	if (!allvars.map)
+	{
+		printf("failed allocation: map\n");
+		return (1);
+	}
+	if (!get_player_coordinates(allvars.map, &allvars.player.player_x, &allvars.player.player_y))
+	{
+		printf("error: get player (x, y)?\n");
+		return (1);
+	}
+	if (!get_exit_coordinates(allvars.map, &allvars.exit.exit_x, &allvars.exit.exit_y))
+	{
+		printf("error: get exit (x, y)?\n");
+		return (1);
+	}
+	if (invalid_map(allvars.map, allvars.player.player_x, allvars.player.player_y))
+	{
+		printf("error: inavlid map\n");
+		return (1);
+	}
+	//invalid map function here!!!!
+	s_data_img[0].xpm_file = ft_strdup("textures/floor.xpm");
+	s_data_img[1].xpm_file = ft_strdup("textures/wall.xpm");
+	s_data_img[2].xpm_file = ft_strdup("textures/jewellery.xpm");
+	s_data_img[3].xpm_file = ft_strdup("textures/exit.xpm");
+	s_data_img[4].xpm_file = ft_strdup("textures/player_left.xpm");
+	s_data_img[5].xpm_file = ft_strdup("textures/player_right.xpm");
 
 	allvars.mlx = mlx_init();
 	if (!allvars.mlx)
@@ -55,7 +76,6 @@ int	main(int ac, char **av)
 		printf("can't create new window!\n");
 		return (1);
 	}
-
 	i = 0;
 	while (i < 6)
 	{
@@ -65,31 +85,12 @@ int	main(int ac, char **av)
 			fill_container_with_grass(allvars.mlx, &s_data_img[0], &s_data_img[i], &allvars.data_img[i], allvars.window_sx, allvars.window_sy, allvars.width_map, allvars.height_map);
 		i++;
 	}
-
-	allvars.map = get_map(av[1], allvars.height_map);
-	if (!allvars.map)
-	{
-		printf("failed allocation: map\n");
-		return (1);
-	}
-
-	if (!get_player_coordinates(allvars.map, &allvars.player.player_x, &allvars.player.player_y))
-	{
-		printf("error: get player (x, y)?\n");
-		return (1);
-	}
-	if (!get_exit_coordinates(allvars.map, &allvars.exit.exit_x, &allvars.exit.exit_y))
-	{
-		printf("error: get exit (x, y)?\n");
-		return (1);
-	}
-
 	if (allvars.width_map / 2 >= allvars.player.player_x)
 		allvars.player.direction = 'R';
 	else
 		allvars.player.direction = 'L';
-	put_map_to_window(&allvars, allvars.player.direction);
-
+	if (!put_map_to_window(&allvars, allvars.player.direction))
+		return (1);
 	mlx_key_hook(allvars.window, key_hook, &allvars);
 	mlx_loop(allvars.mlx);
 	
@@ -175,7 +176,6 @@ int	key_hook(int keycode, t_allvars *vars)
 	{
 		if (keycode == 65307)
 			exit(0);
-		printf("keycode => %d\n", keycode);
 		return (1);
 	}
 	return (0);
